f2ee47cf66e6f8caf36c395d9e22de02828aa1f8
datahive-senugmin <seungmin@datahive.co.kr>
2022-05-26 12:44:23 +0900

    shs

--- dbcon.php
+++ dbcon.php
@@ -1,6 +1,19 @@
 <?php
-$con = mysqli_connect("13.125.248.43", "datahive", "Mobile1!", "shs");
-$sql = "SELECT * FROM shs_admin";
-$result = mysqli_query($con, $sql);
+header("Content-Type: application/json"); //json을 사용하기 위해 필요한 구문
+
+//한글의 경우 PHP json에서 표시가 되지 않는 문제점으로 인해 한글 사용시 iconv로 변환하여 넣어주었음.
+$profile = array("name" => iconv("EUC-KR", "UTF-8", "홍길동"), "age" => "37"); //이름, 나이 정보가 있는 배열
+$contact = array("address" => iconv("EUC-KR", "UTF-8", "광화문 서울특별시 종로구 세종로 사직로 161"), "tel" => "010-1234-5555"); //주소와 전화번호가 정보가 있는 배열
+$career = array("job" => iconv("EUC-KR", "UTF-8", "무사"), "rank" => iconv("EUC-KR", "UTF-8", "팀장")); //직업과 직급의 정보가 있는 배열
+
+//각각의 정보를 하나의 배열 변수에 넣어준다.
+$data = array(
+    "profile" => $profile,
+    "contact" => $contact,
+    "career" => $career
+);
+
+//json 출력
+echo (json_encode($data, JSON_UNESCAPED_UNICODE));
 
 ?>
--- shs-mobile/dbcon.php
+++ shs-mobile/dbcon.php
@@ -1,6 +1,6 @@
 <?php
 $con = mysqli_connect("13.125.248.43", "datahive", "Mobile1!", "shs");
-$sql = "SELECT * FROM shs_admin";
+$sql = "SELECT * FROM shs_admin_estimate";
 $result = mysqli_query($con, $sql);
 
 ?>
